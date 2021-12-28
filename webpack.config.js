567209b1e8794338a8e56290db2be63d38320215
Rif-7 <muhammadrifan974530@gmail.com>
2021-12-28 20:12:41 +0530

    initial branch commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,36 @@
-const path =  require("path");
+const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-
 module.exports = {
-    mode: "production",
-    entry: "./src/index.js",
-    output: {
-        filename: "[name].bundle.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true
-    },
-    plugins: [new HtmlWebPackPlugin({template: "./src/template.html"}),
-              new MiniCssExtractPlugin({filename: "[name].css"}),],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"]
-            }
-        ]
-    }
-}
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({ template: "./src/template.html" }),
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+        },
+      },
+    ],
+  },
+};
