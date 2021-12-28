da11b2cd54d9c5150f666cba30d03915b272b839
Rif-7 <muhammadrifan974530@gmail.com>
2021-12-28 20:27:49 +0530

    changed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,6 @@ module.exports = {
           loader: "babel-loader",
           options: {
             presets: [["@babel/preset-env", { targets: "defaults" }]],
-            plugins: ["@babel/plugin-proposal-class-properties"],
           },
         },
       },
