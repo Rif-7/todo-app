ad3f7a77e68ca83895525eae7a83569cc87ffb4b
Rif-7 <muhammadrifan974530@gmail.com>
2022-02-08 21:06:44 +0530

    add demo functionality and a button to reset everything

--- src/app/localStorageFunctions.js
+++ src/app/localStorageFunctions.js
@@ -1,109 +1,129 @@
-import { compareAsc, format } from 'date-fns';
+import { compareAsc, format } from "date-fns";
 
-function initLocalStorage() {
-    if (!localStorage.getItem("projects")) {
-        localStorage.setItem("projects", JSON.stringify(["Home"]));
-        localStorage.setItem("Home", JSON.stringify([]));
-    }
+function initLocalStorage(showDemo = true) {
+  if (!localStorage.getItem("projects")) {
+    if (showDemo) return setDemo();
+    localStorage.setItem("projects", JSON.stringify(["Home"]));
+    localStorage.setItem("Home", JSON.stringify([]));
+  }
 }
 
 function createProject(title) {
-    initLocalStorage();
-    const projects = JSON.parse(localStorage.getItem("projects"));
-    if (projects.includes(title)) return alert("project already exists");
-    projects.push(title);
-    localStorage.setItem("projects", JSON.stringify(projects));
+  initLocalStorage();
+  const projects = JSON.parse(localStorage.getItem("projects"));
+  if (projects.includes(title)) return alert("project already exists");
+  projects.push(title);
+  localStorage.setItem("projects", JSON.stringify(projects));
 
-    localStorage.setItem(title, JSON.stringify([]));
+  localStorage.setItem(title, JSON.stringify([]));
 }
 
 function saveTodo(todo) {
-    if (!localStorage.getItem(todo.project)) return alert("project doesn't exist");
+  if (!localStorage.getItem(todo.project))
+    return alert("project doesn't exist");
 
-    const project = JSON.parse(localStorage.getItem(todo.project));
-    project.push(JSON.stringify(todo));
+  const project = JSON.parse(localStorage.getItem(todo.project));
+  project.push(JSON.stringify(todo));
 
-    localStorage.setItem(todo.project, JSON.stringify(project));
+  localStorage.setItem(todo.project, JSON.stringify(project));
 }
 
 function loadProjectList() {
-    return JSON.parse(localStorage.getItem("projects"));
+  return JSON.parse(localStorage.getItem("projects"));
 }
 
 function changeTodoState(projectName, todoIndex) {
-    const projectData = loadProject(projectName);
-    const todo  = JSON.parse(projectData[todoIndex]);
-    todo.checked = !todo.checked;
-    projectData[todoIndex] = JSON.stringify(todo);
-    localStorage.setItem(projectName, JSON.stringify(projectData));
-
+  const projectData = loadProject(projectName);
+  const todo = JSON.parse(projectData[todoIndex]);
+  todo.checked = !todo.checked;
+  projectData[todoIndex] = JSON.stringify(todo);
+  localStorage.setItem(projectName, JSON.stringify(projectData));
 }
 
 function loadProject(projectName) {
-    return JSON.parse(localStorage.getItem(projectName));
+  return JSON.parse(localStorage.getItem(projectName));
 }
 
 function loadAllTodos() {
-    const projectNames = loadProjectList();
-    const allTodos = [];
-    for (let i = 0; i < projectNames.length; i++) {
-        const currentProject = loadProject(projectNames[i]);
-        allTodos.push(...currentProject);
-    }
-    return sortTodos(allTodos);
+  const projectNames = loadProjectList();
+  const allTodos = [];
+  for (let i = 0; i < projectNames.length; i++) {
+    const currentProject = loadProject(projectNames[i]);
+    allTodos.push(...currentProject);
+  }
+  return sortTodos(allTodos);
 }
 
 function sortTodos(todos) {
-    return todos
-    .map(todo => {
-        const parsedTodo = JSON.parse(todo);
-        parsedTodo.dueDate = new Date(Date.parse(parsedTodo.dueDate));
-        return parsedTodo;
+  return todos
+    .map((todo) => {
+      const parsedTodo = JSON.parse(todo);
+      parsedTodo.dueDate = new Date(Date.parse(parsedTodo.dueDate));
+      return parsedTodo;
     })
     .sort((a, b) => compareAsc(a.dueDate, b.dueDate));
 }
 
 function loadTodaysTodos() {
-    const allTodos = loadAllTodos();
-    const t =  new Date();
-    const today = new Date(t.getFullYear(), t.getMonth(), t.getDate());
-    const todaysTodos = [];
-    for (let i = 0; i < allTodos.length; i++) {
-        const currentTodo = allTodos[i];
-        const currentTodoDate = new Date(currentTodo.dueDate.getFullYear(),
-                                        currentTodo.dueDate.getMonth(),
-                                        currentTodo.dueDate.getDate());
-        if (currentTodoDate > today) {
-            break;
-        } else if (currentTodoDate.getTime() === today.getTime()) {
-            currentTodo.dueDate = format(currentTodo.dueDate, 'yyyy-MM-dd');
-            todaysTodos.push(currentTodo);
-            continue;
-        }
+  const allTodos = loadAllTodos();
+  const t = new Date();
+  const today = new Date(t.getFullYear(), t.getMonth(), t.getDate());
+  const todaysTodos = [];
+  for (let i = 0; i < allTodos.length; i++) {
+    const currentTodo = allTodos[i];
+    const currentTodoDate = new Date(
+      currentTodo.dueDate.getFullYear(),
+      currentTodo.dueDate.getMonth(),
+      currentTodo.dueDate.getDate()
+    );
+    if (currentTodoDate > today) {
+      break;
+    } else if (currentTodoDate.getTime() === today.getTime()) {
+      currentTodo.dueDate = format(currentTodo.dueDate, "yyyy-MM-dd");
+      todaysTodos.push(currentTodo);
+      continue;
     }
-    return todaysTodos;
+  }
+  return todaysTodos;
 }
 
 function deleteTodo(projectName, index) {
-    const project = loadProject(projectName);
-    project.splice(index, 1);
-    localStorage.setItem(projectName, JSON.stringify(project));
+  const project = loadProject(projectName);
+  project.splice(index, 1);
+  localStorage.setItem(projectName, JSON.stringify(project));
 }
 
 function deleteProject(projectName) {
-    localStorage.removeItem(projectName);
-    const projectList = JSON.parse(localStorage.getItem("projects"));
-    let index;
-    for (let i = 0; i < projectList.length; i++) {
-        if (projectList[i] === projectName) {
-            index = i;
-            break;
-        }
+  localStorage.removeItem(projectName);
+  const projectList = JSON.parse(localStorage.getItem("projects"));
+  let index;
+  for (let i = 0; i < projectList.length; i++) {
+    if (projectList[i] === projectName) {
+      index = i;
+      break;
     }
-    projectList.splice(index, 1);
-    localStorage.setItem("projects", JSON.stringify(projectList));
+  }
+  projectList.splice(index, 1);
+  localStorage.setItem("projects", JSON.stringify(projectList));
 }
 
+function setDemo() {
+  const demoData = require("./json/demo.json");
+  const { projects, Home, School } = demoData;
+  localStorage.setItem("projects", JSON.stringify(projects));
+  localStorage.setItem("Home", JSON.stringify(Home));
+  localStorage.setItem("School", JSON.stringify(School));
+}
 
-export {createProject, saveTodo, loadProject, loadTodaysTodos, deleteProject,
-     loadProjectList, initLocalStorage, changeTodoState, deleteTodo};
+export {
+  createProject,
+  saveTodo,
+  loadProject,
+  loadTodaysTodos,
+  deleteProject,
+  loadProjectList,
+  initLocalStorage,
+  changeTodoState,
+  deleteTodo,
+  setDemo,
+};
--- src/index.js
+++ src/index.js
@@ -1,59 +1,99 @@
-import "./style.css"
-import {createProject, saveTodo, loadProjectList, initLocalStorage} from "./app/localStorageFunctions";
-import {loadProjectsToDom, setModal, loadNewTodoDetails,
-     reloadTodos, loadTodaysTodosToDom, deleteProjectFromDom} from "./app/DomFunctions"
+import "./style.css";
+import {
+  createProject,
+  saveTodo,
+  loadProjectList,
+  initLocalStorage,
+  setDemo,
+} from "./app/localStorageFunctions";
+import {
+  loadProjectsToDom,
+  setModal,
+  loadNewTodoDetails,
+  reloadTodos,
+  loadTodaysTodosToDom,
+  deleteProjectFromDom,
+} from "./app/DomFunctions";
 
-function todoItem(title, dueDate, description, priority, project="default", checked=false) {
-    this.title = title
-    this.dueDate = dueDate
-    this.description = description
-    this.priority = priority
-    this.checked = checked
-    this.project = project
+function todoItem(
+  title,
+  dueDate,
+  description,
+  priority,
+  project = "default",
+  checked = false
+) {
+  this.title = title;
+  this.dueDate = dueDate;
+  this.description = description;
+  this.priority = priority;
+  this.checked = checked;
+  this.project = project;
+}
+
+function makeChange(showDemo = true) {
+  document.querySelector(".project-name").click();
+  localStorage.clear();
+  initLocalStorage(showDemo);
+  loadProjectsToDom();
+  reloadTodos();
+  document.querySelector(".project-name").click();
 }
 
 const newProjectBtn = document.querySelector(".create-project");
 newProjectBtn.addEventListener("click", () => {
-    const title = document.querySelector(".project-title").value;
-    if (!title) {
-        alert("Project citle can't be empty");
-        return;
-    }
-    createProject(title);
-    loadProjectsToDom(loadProjectList());
-    document.querySelector(".project-title").value = "";
+  const title = document.querySelector(".project-title").value;
+  if (!title) {
+    alert("Project citle can't be empty");
+    return;
+  }
+  createProject(title);
+  loadProjectsToDom(loadProjectList());
+  document.querySelector(".project-title").value = "";
 });
 
 const saveBtn = document.querySelector(".save-todo");
-saveBtn.addEventListener("click", function() {
-    const todoDetails = loadNewTodoDetails();
-    const todo = new todoItem(todoDetails.title, todoDetails.dueDate, todoDetails.description,
-        todoDetails.priority, todoDetails.project);
-    saveTodo(todo);
-    reloadTodos();
-    document.querySelector("#todo-title").value = "";
-    document.querySelector("#todo-description").value = "";
-    document.querySelector(".close").click();
+saveBtn.addEventListener("click", function () {
+  const todoDetails = loadNewTodoDetails();
+  const todo = new todoItem(
+    todoDetails.title,
+    todoDetails.dueDate,
+    todoDetails.description,
+    todoDetails.priority,
+    todoDetails.project
+  );
+  saveTodo(todo);
+  reloadTodos();
+  document.querySelector("#todo-title").value = "";
+  document.querySelector("#todo-description").value = "";
+  document.querySelector(".close").click();
 });
 
-document.querySelector(".details-close").addEventListener("click", function() {
-    document.querySelector("#details-modal").style.display = "none";
+document.querySelector(".details-close").addEventListener("click", function () {
+  document.querySelector("#details-modal").style.display = "none";
 });
 
-document.querySelector(".today-todo").addEventListener("click", loadTodaysTodosToDom);
-document.querySelector("#delete-project").addEventListener("click", function() {
+document
+  .querySelector(".today-todo")
+  .addEventListener("click", loadTodaysTodosToDom);
+document
+  .querySelector("#delete-project")
+  .addEventListener("click", function () {
     deleteProjectFromDom();
     document.querySelector(".project-name").click();
+  });
+
+document.querySelector(".reset-btn").addEventListener("click", () => {
+  makeChange(false);
 });
 
+document.querySelector(".show-demo").addEventListener("click", () => {
+  makeChange();
+});
 
 initLocalStorage();
 loadProjectsToDom();
 setModal();
 
-
-// The first project-name will be Home 
+// The first project-name will be Home
 document.querySelector(".project-name").click();
-
-
-
